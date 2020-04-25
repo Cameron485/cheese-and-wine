289d3845bd5aafd8b225d591e67b3e1dbf867d32
Cameron485 <cfindl15@caledonian.ac.uk>
2020-04-25 11:35:45 +0100

    Add thanks page

--- src/pages/thanks/index.js
+++ src/pages/thanks/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+import breakPoints from '../../helpers/breakPoints';
 import { Layout } from '../../components';
+
+const StyledThanksContainer = styled.div`
+    height: 80vh;
+    font-size: 2rem;
+
+    @media (max-width: ${breakPoints.tablet}px) {
+        height: 60vh;
+        font-size: 1rem;
+    }
+`;
 
 const PeoplePage = () => (
     <Layout>
-        <div className="container">
-            <p>Thanks</p>
-        </div>
+        <StyledThanksContainer className="section">
+            <div className="container">
+                <div className="content has-text-centered">
+                    <h1 className="has-text-weight-bold is-size-1">
+                        Cheers Pal!
+                    </h1>
+                    <p>
+                        Got some more? Click <Link to="/">here</Link> to go back
+                    </p>
+                </div>
+            </div>
+        </StyledThanksContainer>
     </Layout>
 );
 
